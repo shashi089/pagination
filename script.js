7db34e3d28e9968f03c829a982f718d394785ae7
shashidharnaik55 <shashidharnaik55@gmail.com>
2021-08-21 15:16:19 +0530

    Pagination task submitted

---
+++ script.js
@@ -0,0 +1,72 @@
+var userData; // data to be retrieved from the json file
+// read JSON file
+function readTextFile(file, callback) {
+  var rawFile = new XMLHttpRequest();
+  rawFile.overrideMimeType('application/json');
+  rawFile.open('GET', file, true);
+  rawFile.onreadystatechange = function () {
+    if (rawFile.readyState === 4 && rawFile.status == '200') {
+      callback(rawFile.responseText);
+    }
+  };
+  rawFile.send(null);
+}
+
+readTextFile('./user.json', function (data) {
+  userData = JSON.parse(data);
+});
+
+var oldPage = 0;
+
+// To display records
+
+function displayPage(pageNum) {
+  var startIndex = (pageNum - 1) * 10;
+  var endIndex = startIndex + 10;
+  var pageWiseUserData = userData.slice(startIndex, endIndex);
+
+  for (var i = 0; i < pageWiseUserData.length; i++) {
+    createUser(pageWiseUserData, i);
+  }
+
+  // To create data table
+  function createUser(pageWiseUserData, i) {
+    const info = document.createElement('tr');
+    var outputTable = document.getElementById('userdetails');
+    info.innerHTML = `
+     
+      <td>${pageWiseUserData[i].id}</td>
+      <td>${pageWiseUserData[i].name}</td>
+      <td>${pageWiseUserData[i].email}</td>
+      `;
+    outputTable.appendChild(info);
+    console.log(info);
+  }
+}
+
+// To get page Number
+document.querySelector('ul').onclick = function (e) {
+  var pageNum = e.target.textContent;
+  console.log(pageNum);
+  var acheck = document.querySelector('ul li a.active'); // to check active Page Number
+  if (acheck) {
+    document.querySelector('ul li a.active').classList.remove('active');
+  }
+  e.target.classList.add('active');
+
+  var outputTable = document.getElementById('userdetails');
+  outputTable.innerHTML = ''; // to empty the table
+  // First Page
+  if (pageNum === 'First') {
+    pageNum = 1;
+  }
+  // Next Page
+  else if (pageNum === 'Next') {
+    pageNum = oldPage + 1;
+  }
+  if (pageNum > 10) {
+    pageNum = 1;
+  }
+  oldPage = +pageNum;
+  displayPage(pageNum);
+};
